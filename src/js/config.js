efe0cc792a7001d284abbdb49ebbc8dee1a5a701
Ben-Besliel <benbeslield@gmailcom>
2022-07-29 19:01:55 +0530

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'e56762dc-acb8-4f2c-a86d-df37067aa28f';
+export const MODAL_CLOSE_SEC = 2.5;
